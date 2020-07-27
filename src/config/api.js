30ce7e2d9a1eb87cc8af9ceec833914524f12480
Phillip <=>
2020-07-27 23:11:49 +1000

    Revert "heroku base url updated"
    This reverts commit 41ace16e3ddd83c42485573e8a24b7fe2ef2864f.

--- src/config/api.js
+++ src/config/api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 export default axios.create(
     {
-        // baseURL: 'http://localhost:3001',
-        baseURL: "https://socialzr-server.herokuapp./com/",
+        baseURL: 'http://localhost:3001',
+        // baseURL: "https://socialzr-server.herokuapp./com/",
         timeout: 5000,
         withCredentials: true
     })
