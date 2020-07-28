be9a225ccd222e8672796f7cc9678ae9f67094e8
Phillip <=>
2020-07-28 16:01:52 +1000

    base url switched to heroku url

--- src/config/api.js
+++ src/config/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default axios.create(
     {
         // baseURL: 'http://localhost:3001',
-        baseURL: "https://socialzr-server.herokuapp./com/",
+        baseURL: "https://socialzr-server.herokuapp.com/",
         timeout: 5000,
         withCredentials: true
     })
