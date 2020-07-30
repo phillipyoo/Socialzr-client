2d99dfb822747eb791ed8bdf0c233c9d944444dd
thestrongcoder <stephleehales@gmail.com>
2020-07-31 03:51:21 +1000

    images and header changes

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,16 +1,17 @@
   
 import React from 'react';
-import Header from '../components/Header'
+// import Header from '../components/Header'
 import ContentsMain from '../components/ContentsMain'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 import '../styles/App.css'
 import '../styles/theme.css'
+import imageHeader from '../img/socialzr-band.png'
 
 function Index() {
     return (
       <React.Fragment>
-        <Header />
+        <img id="headerImage" src={imageHeader} alt=" " />
         <ContentsMain />
         {/* <UpcomingEvents /> */}
         <Contact />
